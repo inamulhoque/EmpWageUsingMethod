5f38cf362955b7528c66eda6a709bf5353ee1e35
Inamul <inamulhoque097@gmail.com>
2022-06-02 13:36:50 +0530

    Added: Part time employee and calculate daily wage

--- src/com/inamul/EmployeeWage.java
+++ src/com/inamul/EmployeeWage.java
@@ -5,9 +5,13 @@ public class EmployeeWage {
         int empWage =0;
         int Wage_Per_Hr = 20;
         int emphrs=0;
-        int empCheck = (int) Math.floor(Math.random() * 10) % 2+1;
+
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3+1;
         if (empCheck == 1){
             System.out.println("Employee is absent.");
+        } else if (empCheck == 2) {
+            System.out.println("Employee present part time.");
+            emphrs=4;
         } else {
             System.out.println("Employee is present.");
             emphrs=8;
