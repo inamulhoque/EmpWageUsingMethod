8bb6df670511fbf99a447a63be56a347fa4cfa0c
Inamul <inamulhoque097@gmail.com>
2022-06-02 13:34:10 +0530

    Added: Daily wage.

--- src/com/inamul/EmployeeWage.java
+++ src/com/inamul/EmployeeWage.java
@@ -2,12 +2,18 @@ package com.inamul;
 
 public class EmployeeWage {
     public static void employee(){
+        int empWage =0;
+        int Wage_Per_Hr = 20;
+        int emphrs=0;
         int empCheck = (int) Math.floor(Math.random() * 10) % 2+1;
         if (empCheck == 1){
             System.out.println("Employee is absent.");
         } else {
             System.out.println("Employee is present.");
+            emphrs=8;
         }
+        empWage = Wage_Per_Hr*emphrs;
+        System.out.println("Daily wage is: "+"$"+empWage);
     }
     public static void main(String[] args) {
         EmployeeWage.employee();
