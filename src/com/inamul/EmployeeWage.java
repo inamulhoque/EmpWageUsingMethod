ac46bed82e8e36c0705cef8e80c757533a23bc9f
Inamul <inamulhoque097@gmail.com>
2022-06-02 13:56:12 +0530

    Added: Maximum working day = 20 and hours = 100

--- src/com/inamul/EmployeeWage.java
+++ src/com/inamul/EmployeeWage.java
@@ -9,25 +9,29 @@ public class EmployeeWage {
         int day = 0;
         int totalWage = 0;
         int Total_Working_Hour = 0;
+        int Max_Working_Hour = 100;
 
-        while (day < Max_Working_Day) {
+        while (day < Max_Working_Day && Total_Working_Hour<Max_Working_Hour) {
             int empCheck = (int) Math.floor(Math.random() * 10) % 3 + 1;
             switch (empCheck) {
                 case 1:
                     emphrs = 8;
+                    day++;
                     break;
                 case 2:
                     emphrs = 4;
+                    day++;
                     break;
                 case 3:
                     emphrs = 0;
                     break;
             }
-            day++;
             Total_Working_Hour = Total_Working_Hour + emphrs;
             totalWage = Wage_Per_Hr * Total_Working_Hour;
         }
         System.out.println("Monthly wage of the employee is: "+totalWage);
+        System.out.println("Total days worked: "+day);
+        System.out.println("Total hours worked: "+Total_Working_Hour);
     }
     public static void main(String[] args) {
         EmployeeWage.employee();
