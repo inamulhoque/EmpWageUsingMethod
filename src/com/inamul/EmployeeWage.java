ba004883b94713f5e1bacfed3d9baef8293965b2
Inamul <inamulhoque097@gmail.com>
2022-06-02 13:48:36 +0530

    Added: Monthly wage

--- src/com/inamul/EmployeeWage.java
+++ src/com/inamul/EmployeeWage.java
@@ -5,24 +5,29 @@ public class EmployeeWage {
         int empWage = 0;
         int Wage_Per_Hr = 20;
         int emphrs = 0;
+        int Max_Working_Day = 20;
+        int day = 0;
+        int totalWage = 0;
+        int Total_Working_Hour = 0;
 
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3 + 1;
-        switch (empCheck) {
-            case 1:
-                System.out.println("Employee is present full time");
-                emphrs = 8;
-                break;
-            case 2:
-                System.out.println("Employee is present part time");
-                emphrs = 4;
-                break;
-            case 3:
-                System.out.println("Employee is absent");
-                emphrs = 0;
-                break;
+        while (day < Max_Working_Day) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3 + 1;
+            switch (empCheck) {
+                case 1:
+                    emphrs = 8;
+                    break;
+                case 2:
+                    emphrs = 4;
+                    break;
+                case 3:
+                    emphrs = 0;
+                    break;
+            }
+            day++;
+            Total_Working_Hour = Total_Working_Hour + emphrs;
+            totalWage = Wage_Per_Hr * Total_Working_Hour;
         }
-        empWage = Wage_Per_Hr * emphrs;
-        System.out.println("Daily wage is: " + "$" + empWage);
+        System.out.println("Monthly wage of the employee is: "+totalWage);
     }
     public static void main(String[] args) {
         EmployeeWage.employee();
