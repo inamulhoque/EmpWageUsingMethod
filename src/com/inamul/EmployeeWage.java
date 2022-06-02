6b626f1f46bc7f0dfc90673a8c60af8c5d2c23f4
Inamul <inamulhoque097@gmail.com>
2022-06-02 13:41:37 +0530

    Added: Use Switch case

--- src/com/inamul/EmployeeWage.java
+++ src/com/inamul/EmployeeWage.java
@@ -1,25 +1,31 @@
 package com.inamul;
 
 public class EmployeeWage {
-    public static void employee(){
-        int empWage =0;
+    public static void employee() {
+        int empWage = 0;
         int Wage_Per_Hr = 20;
-        int emphrs=0;
+        int emphrs = 0;
 
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3+1;
-        if (empCheck == 1){
-            System.out.println("Employee is absent.");
-        } else if (empCheck == 2) {
-            System.out.println("Employee present part time.");
-            emphrs=4;
-        } else {
-            System.out.println("Employee is present.");
-            emphrs=8;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3 + 1;
+        switch (empCheck) {
+            case 1:
+                System.out.println("Employee is present full time");
+                emphrs = 8;
+                break;
+            case 2:
+                System.out.println("Employee is present part time");
+                emphrs = 4;
+                break;
+            case 3:
+                System.out.println("Employee is absent");
+                emphrs = 0;
+                break;
         }
-        empWage = Wage_Per_Hr*emphrs;
-        System.out.println("Daily wage is: "+"$"+empWage);
+        empWage = Wage_Per_Hr * emphrs;
+        System.out.println("Daily wage is: " + "$" + empWage);
     }
     public static void main(String[] args) {
         EmployeeWage.employee();
     }
 }
+
