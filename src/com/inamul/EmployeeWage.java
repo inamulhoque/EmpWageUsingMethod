9f312f7a3917115e9bb217b22e0d3ec40ee93f18
Inamul <inamulhoque097@gmail.com>
2022-06-02 13:01:20 +0530

    Added: Employee absent or present.

--- src/com/inamul/EmployeeWage.java
+++ src/com/inamul/EmployeeWage.java
@@ -1,7 +1,15 @@
 package com.inamul;
 
 public class EmployeeWage {
+    public static void employee(){
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2+1;
+        if (empCheck == 1){
+            System.out.println("Employee is absent.");
+        } else {
+            System.out.println("Employee is present.");
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+        EmployeeWage.employee();
     }
 }
